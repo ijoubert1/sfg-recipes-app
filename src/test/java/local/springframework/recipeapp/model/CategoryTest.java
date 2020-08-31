1e0c6de72ca90e65c4744e3f5bc652b2cc10bdfe
ijoubert1 <irian.joubert@gmail.com>
2020-08-31 15:02:01 -0300

    Closes #14

---
+++ src/test/java/local/springframework/recipeapp/model/CategoryTest.java
@@ -0,0 +1,23 @@
+package local.springframework.recipeapp.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long id = 4l;
+        category.setId(id);
+        assertEquals(id, category.getId());
+    }
+}
