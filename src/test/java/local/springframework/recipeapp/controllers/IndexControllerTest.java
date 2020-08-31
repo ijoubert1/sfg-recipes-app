718f9ef4986b3f7e98d897c356597cf1041d7f1e
ijoubert1 <irian.joubert@gmail.com>
2020-08-31 16:42:02 -0300

    Closes #17

--- src/test/java/local/springframework/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/local/springframework/recipeapp/controllers/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package local.springframework.recipeapp.controllers;
 
+import local.springframework.recipeapp.model.Recipe;
 import local.springframework.recipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -28,10 +33,28 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
-        String result = indexController.getIndexPage(model);
-        assertNotNull(result);
-        assertEquals("index", result);
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1l);
+
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2l);
+
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+        assertNotNull(viewName);
+        assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
